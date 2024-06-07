9110dfc396cbc95bc559165ae35dc4530aec49f6
GerinNugroho <gerinnugroho17@gmail.com>
2024-06-07 10:14:25 +0700

    Button Commit

--- js/script.js
+++ js/script.js
@@ -2,16 +2,23 @@ const scriptURL = 'https://script.google.com/macros/s/AKfycbxEgFQLi5gP2jbh-snwzE
 const form = document.forms['Form-Comment'];
 const Name = document.getElementById('Name');
 const Comment = document.getElementById('Comment');
+const button = document.querySelector('.btn-send');
 
 form.addEventListener('submit', (e) => {
   e.preventDefault();
+  button.innerHTML = `Wait...`;
+  button.classList.add('disabled');
   fetch(scriptURL, { method: 'POST', body: new FormData(form) })
     .then((response) => {
       form.reset();
-      alert('Succes!');
+      button.classList.remove('disabled');
+      button.innerHTML = `<i class="bi bi-send-fill"></i>Send`;
+      alert('Sending Success!');
     })
     .catch((error) => {
       form.reset();
-      alert('Failed!');
+      button.classList.remove('disabled');
+      button.innerHTML = `<i class="bi bi-send-fill"></i>Send`;
+      alert('Sending Failed');
     });
 });
